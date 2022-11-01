5621e9cbb5d3fb6b7cd8324518ebfccd3436cce3
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2022-11-01 09:59:17 +0300

    HW full

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -29,18 +27,74 @@ public class Main {
         map1.addMap("str1", 5);
         System.out.println(map1);
 
-        Map<String, List<Integer>> map2 = new HashMap<>();
-        map2.put("str1",List.of(1,2,3));
-        map2.put("str2",List.of(4,5,6));
-        map2.put("str3",List.of(7,8,9));
-        map2.put("str4",List.of(1,5,8));
-        map2.put("str5",List.of(9,3,4));
+        Map<String, List<Integer>> map2 = new HashMap<>();     /////////////// Домашняя работа 2. Задание 1
+        List<Integer> list1 = new LinkedList<>();
+        Random random = new Random();
+        for (int i = 0; i < 3; i++) {
+            list1.add(random.nextInt(9));
+        }
+
+        List<Integer> list2 = new LinkedList<>();
+        for (int i = 0; i < 3; i++) {
+            list2.add(random.nextInt(9));
+        }
+
+        List<Integer> list3 = new LinkedList<>();
+        for (int i = 0; i < 3; i++) {
+            list3.add(random.nextInt(9));
+        }
+
+        List<Integer> list4 = new LinkedList<>();
+        for (int i = 0; i < 3; i++) {
+            list4.add(random.nextInt(9));
+        }
+
+        List<Integer> list5 = new LinkedList<>();
+        for (int i = 0; i < 3; i++) {
+            list5.add(random.nextInt(9));
+        }
+
+        map2.put("str1",list1);
+        map2.put("str2",list2);
+        map2.put("str3",list3);
+        map2.put("str4",list4);
+        map2.put("str5",list5);
         System.out.println(map2);
 
+        int sum1 = getSum(list1);
+        int sum2 = getSum(list2);
+        int sum3 = getSum(list3);
+        int sum4 = getSum(list4);
+        int sum5 = getSum(list5);
 
         Map<String, Integer> map3 = new HashMap<>();
+        map3.put("Str1", sum1);
+        map3.put("Str2", sum2);
+        map3.put("Str3", sum3);
+        map3.put("Str4", sum4);
+        map3.put("Str5", sum5);
+        System.out.println(map3);
 
+        Map<Integer, String> map4 = new LinkedHashMap<>();   //////////Домашнее Задание 2. Задача 2
+        map4.put(1, "str1");
+        map4.put(2, "str2");
+        map4.put(3, "str3");
+        map4.put(4, "str4");
+        map4.put(5, "str5");
+        map4.put(6, "str6");
+        map4.put(7, "str7");
+        map4.put(8, "str8");
+        map4.put(9, "str9");
+        map4.put(10, "str10");
+        System.out.println(map4);
 
+    }
 
+    public static int getSum(List<Integer> nums) {   //////// для ДЗ2.1.
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        return sum;
     }
 }
